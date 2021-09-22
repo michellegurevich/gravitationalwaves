89664065e6482899e53351f182d5dd5ee9e2a65f
michgurevich <mgur@umich.edu>
2021-09-22 16:48:30 +0100

    all terms defined, mod amp works for f < f_max case, array needs to be adjusted for interesting case

--- modgravity/ModifiedPolarization.py
+++ modgravity/ModifiedPolarization.py
@@ -1,13 +1,17 @@
 import math
 from CalculateDistances import CalculateDistances
 
+
 class ModifiedPolarization:
     h = 1  # Planck constant
     f_e = 10 ** 4  # Hz
     lambda_g = 1.6 * 10 ** 16  # m
+    epsilon = math.sqrt(3) / 2  # for LISA, for LIGO would equal 1
 
     E_e = h * f_e
     m_g = h / lambda_g
+    t_c = 1
+    phi_c = 1
 
     h0 = 72
     omega_m = .3
@@ -53,3 +57,32 @@ class ModifiedPolarization:
         else:
             term_ii = cls.zeta(z, alpha, A_term, chirp_mass) * (cls.u(chirp_mass, f) ** (alpha - 1))
         return -1 * (term_i + term_ii)
+
+    @classmethod
+    def curved_A(cls, chirp_mass, z):
+        CD = CalculateDistances()
+        D_L = CD.lum_dist(z)
+        return math.sqrt(math.pi/30) * (chirp_mass ** 2 / D_L)
+
+    @classmethod
+    def mod_amplitude(cls, chirp_mass, z, f):
+        """ tilde A(f) term """
+        return cls.epsilon * cls.curved_A(chirp_mass, z) * (cls.u(chirp_mass, f) ** (-7/6))
+
+    @classmethod
+    def psi_gr(cls, f, chirp_mass):  # , t_c, phi_c):
+        freq_term = 2 * math.pi * f * cls.t_c
+        numerical_term = 3 / 128 * (cls.u(chirp_mass, f) ** -5/3)  # ADD CROSS PRODUCT
+        return freq_term - cls.phi_c - math.pi/4 + numerical_term
+
+    @classmethod
+    def psi(cls, alpha, A_term, chirp_mass, z, f):
+        psi_gr = cls.psi_gr(f, chirp_mass)  # , t_c, phi_c)
+        delta_psi = cls.delta_psi(alpha, A_term, chirp_mass, z, f)
+        return psi_gr + delta_psi
+
+    @classmethod
+    def mod_polarization(cls, f, f_max, chirp_mass, z, alpha, A_term):
+        # needs test to check f > 0
+        return cls.mod_amplitude(chirp_mass, z, f) * math.exp(1j * cls.psi(alpha, A_term, chirp_mass, z, f)) if \
+            f < f_max else 0
--- modgravity/main.py
+++ modgravity/main.py
@@ -11,34 +11,25 @@ def main():
     z_max = 4
     z = np.linspace(0, z_max)
 
-    h0 = 72  # Hubble parameter today in km/s/Mpc
-    omega_m = .3
-    omega_lambda = .7
     alpha = 3
     A_term = .0001
-
     chirp_mass = 25e30  # kg
     f = 10e-3  # Hz
 
-    h = 1
-    f_e = 10**4  # Hz
-    l_g = 1.6*10**16  # m
-    E_e = h * f_e
-    m_g = h / l_g
-    lambda_A_term = h * A_term ** (1 / (alpha - 2))  # always has units of length irrespective of alpha value
-
-    CD = CalculateDistances()
+    # CD = CalculateDistances()
     # lum_dist = CD.lum_dist_array(z_max, z)
     # alpha_dist = CD.alpha_dist_array(z_max, z, 3)
     # print(lum_dist, alpha_dist)
 
-    P = Plots()
+    # P = Plots()
     # P.scale_factor(z)
     # P.alpha_lum_ratios(z_max, z)
     # P.chi_to_mod_chi_ratio(z, m_g, E_e)
 
     MP = ModifiedPolarization()
     print(MP.delta_psi(alpha, A_term, chirp_mass, z, f))
+    print(MP.mod_polarization(f, 10e-4, chirp_mass, z, alpha, A_term))  # f_max < f WORKS FINE (returns 0)
+    print(MP.mod_polarization(f, 10e-2, chirp_mass, z, alpha, A_term))  # f_max > f
 
 if __name__ == '__main__':
     main()
