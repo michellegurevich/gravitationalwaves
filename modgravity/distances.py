175f2deb17be42aafa8a9f01bab91b934892d6c5
Michelle Gurevich <michellegurevich@Michelles-MacBook-Pro.local>
2022-04-11 18:26:07 +0100

    chi plot

--- modgravity/distances.py
+++ modgravity/distances.py
@@ -8,24 +8,23 @@ H0 = 72  # km/s/Mpc
 OMEGA_M = .3
 OMEGA_LAMBDA = .7
 
-
 class distances:
 
     def __init__(self, cosmo_params, phenom_params):
-        self.cosmo_params = cosmo_params
-        self.phenom_params = phenom_params
+        self.cosmo_params   = cosmo_params
+        self.phenom_params  = phenom_params
 
         # unpack cosmo parameters
-        self.z      = cosmo_params.get('redshift', np.linspace(0,0))
-        self.f      = cosmo_params.get('frequency', np.linspace(0,0))
-        self.E_e    = H_PLANCK * cosmo_params.get('f_e', 1)
-        self.t_e    = cosmo_params.get('t_e', 1)
-        self.t_a    = cosmo_params.get('t_a', 1)
+        self.z              = cosmo_params.get('redshift', np.linspace(0,0))
+        self.f              = cosmo_params.get('frequency', np.linspace(0,0))
+        self.E_e            = H_PLANCK * cosmo_params.get('f_e', 1)
+        self.t_e            = cosmo_params.get('t_e', 1)
+        self.t_a            = cosmo_params.get('t_a', 1)
 
         # unpack phenomenological parameters
-        self.A      = phenom_params.get('a', 0)
-        self.alpha  = phenom_params.get('alpha', 0)
-        self.m_g    = H_PLANCK / phenom_params.get('lambda_g', 1)
+        self.A              = phenom_params.get('A', 0)
+        self.alpha          = phenom_params.get('alpha', 0)
+        self.m_g            = H_PLANCK / phenom_params.get('lambda_g', 1)
 
     def __calc_luminosity(self, z):
         integrand = lambda z_prime: 1 / np.sqrt(OMEGA_M * (1 + z_prime) ** 3 + OMEGA_LAMBDA)
@@ -48,22 +47,21 @@ class distances:
     def __calc_chi_term(self, z, E_e, t_e, t_a, alpha, A, m_g):
         # a(t_e) = a(t) evaluated at t = t_e, for redshift at time of emission
         a_t_e = 1 / (1 + z)
-
         # z := a0 (=1) / a(t_e) - 1 = 1 / a(t_e) - 1 => 1 / (z + 1) = a(t_e)
-
         # a(t) = 1 / (1 + z) => 1 / a(t) = 1 + z
         term_I = quad(lambda t: 1 + t * z, t_e, t_a)[0]
-
         # debugger value = 0.124999 (verified on wolfram)
         II_constant = 1 / 2 * m_g ** 2 / (a_t_e ** 2 * E_e ** 2)
         II_integral = quad(lambda t: 1 / (1 + (t * z)), t_e, t_a)
         # debugger value = 4.882812499999999e-40
         term_II = II_constant * II_integral[0]
-
         III_constant = 1 / 2 * A * (a_t_e * E_e) ** (alpha - 2)
         III_integral = quad(lambda t: (1 / (1 + (t * z))) ** (1 - alpha), t_e, t_a)
         term_III = III_constant * III_integral[0]
         return term_I - term_II - term_III
 
     def chi_term(self, z):
+        return np.array([self.__calc_chi_term(z_i, self.E_e, self.t_e, self.t_a, alpha=2, A=0, m_g=0) for z_i in np.nditer(z)])
+
+    def mod_chi_term(self, z):
         return np.array([self.__calc_chi_term(z_i, self.E_e, self.t_e, self.t_a, self.alpha, self.A, self.m_g) for z_i in np.nditer(z)])
--- modgravity/figures.py
+++ modgravity/figures.py
@@ -1,8 +1,8 @@
-from matplotlib import pyplot as plt
 import numpy as np
 import cmath
 import math
-
+from matplotlib import pyplot as plt
+from matplotlib import colors
 from distances import distances
 from set_cosmology import set_cosmology
 from waveforms import waveforms
@@ -10,8 +10,7 @@ from waveforms import waveforms
 
 H_PLANCK = 1  # Planck constant, natural units
 
-class plots:
-
+class figures:
 
     def __init__(self, cosmo_params, phenom_params, wf_params):
         self.cosmo_params   = cosmo_params
@@ -22,16 +21,16 @@ class plots:
         self.wf             = waveforms(cosmo_params, phenom_params, wf_params)
 
         # unpack cosmo parameters
-        self.z      = cosmo_params.get('redshift', np.linspace(0,0))
-        self.f      = cosmo_params.get('frequency', np.linspace(0,0))
-        self.E_e    = H_PLANCK * cosmo_params.get('f_e', 1)
-        self.t_e    = cosmo_params.get('t_e', 1)
-        self.t_a    = cosmo_params.get('t_a', 1)
+        self.z              = cosmo_params.get('redshift', np.linspace(0,0))
+        self.f              = cosmo_params.get('frequency', np.linspace(0,0))
+        self.E_e            = H_PLANCK * cosmo_params.get('f_e', 1)
+        self.t_e            = cosmo_params.get('t_e', 1)
+        self.t_a            = cosmo_params.get('t_a', 1)
 
         # unpack phenomenological parameters
-        self.A      = phenom_params.get('a', 0)
-        self.alpha  = phenom_params.get('alpha', 0)
-        self.m_g    = H_PLANCK / phenom_params.get('lambda_g', 1)
+        self.A              = phenom_params.get('A', 0)
+        self.alpha          = phenom_params.get('alpha', 0)
+        self.m_g            = H_PLANCK / phenom_params.get('lambda_g', 1)
 
         # unpack waveform parameters
         self.approximant    = wf_params.get('TaylorF2', None)
@@ -49,40 +48,35 @@ class plots:
         plt.title('$H(t)$ as a function of redshift')
         return plt.show()
 
-    @staticmethod
-    def alpha_lum_ratios(z_max, z):
+    def alpha_distance_ratio(self):
         """ plot ratios of (modified) alpha and (standard) luminosity distances against redshift """
         alpha_values = np.arange(0, 4.5, .5)  # alpha values end at 4, per GR tests paper specs
-
         for a in alpha_values:
-            lum_dist_array = cls.CD.lum_dist_array(z_max, z)
-            alpha_dist_array = cls.CD.alpha_dist_array(z_max, z, a)
-            results = np.divide(alpha_dist_array[1:], lum_dist_array[1:])  # division by zero error, skip first element
+            D_l = self.dist.luminosity(self.z)
+            D_a = self.dist.mod_luminosity(self.z, a)
+            results = np.divide(D_l[1:], D_a[1:])  # division by zero error, skip first element
             # results = np.insert(results, 0, 0, axis=None)
-            plt.plot(np.hstack(z[1:]), np.hstack(results), label=r'$\alpha$ = '+str(a))
+            plt.plot(np.hstack(np.linspace(0,12)[1:]), np.hstack(results), label=r'$\alpha$ = '+str(a))#, c=colors.Colormap('coolwarm'))
 
         plt.xlabel(r'$z$')
-        plt.ylabel(r'$D_{\alpha} / D_L {Mpc}$')
-        plt.title('Comparison of modified luminosity distances')
-        # handles, labels = plt.gca().get_legend_handles_labels()
-        # plt.gca().legend(handles=handles[::-1], labels=labels[::-1])
-        plt.legend(fontsize='small')
+        plt.ylabel(r'$D_L / D_{\alpha}$ Mpc')
+        plt.title('Modified luminosity distance')
+            # handles, labels = plt.gca().get_legend_handles_labels()
+            # plt.gca().legend(handles=handles[::-1], labels=labels[::-1])
+        plt.legend(bbox_to_anchor=(1.04,1), loc="upper left")
         return plt.show()
 
-    @staticmethod
-    def chi_to_mod_chi_ratio(z_max, z, m_g, E_e):
+    def chi_ratio(self):
         """ plot the ratio of conformal distance chi with and without modification terms """
-        alpha = 3
-        eta_dsrt = 1 * 10e-35  # parameter of order of Planck length
-        chi = cls.CD.chi_array(z_max, 0, 0, m_g, E_e)
-        mod_chi = cls.CD.chi_array(z, alpha, .00002, m_g, E_e, 2, 10)
-        print(chi, mod_chi)
+        eta_dsrt = 1 * 10e-35  # parameter of order Planck length
+        chi = self.dist.chi_term(self.z)
+        mod_chi = self.dist.mod_chi_term(self.z)
 
-        plt.plot(z, chi / mod_chi)
+        plt.plot(self.z, chi / mod_chi)
         plt.xlabel(r'$z$')
         plt.ylabel(r'$\chi_{e | A,\alpha=0} / \chi_e$')
         plt.title('Ratio of standard to modified conformal distance for 'r'$\alpha$'' = '
-                  + str(alpha) + ', A = ' + str(A))  # Double Special Relativity')
+                  + str(self.alpha) + ', A = ' + str(self.A))  # Double Special Relativity')
         return plt.show()
 
     @staticmethod
